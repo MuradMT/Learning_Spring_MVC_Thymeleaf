a587763c226e4b5def37f3b8fe62855090fdc223
Murad Mammadzada <92033053+MuradMT@users.noreply.github.com>
2025-06-24 14:18:36 +0100

    Added all essential files

---
+++ src/main/java/com/example/learning_spring_mvc_thymeleaf/LearningSpringMvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.learning_spring_mvc_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringMvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringMvcThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learning_spring_mvc_thymeleaf/LearningSpringMvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learning_spring_mvc_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringMvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
